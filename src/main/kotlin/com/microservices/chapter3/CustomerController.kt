ed7d88a97314392cde52327b8def59ef90221925
Victor Monte <victorbcm@hotmail.com>
2018-07-10 21:22:46 -0300

    Alter get customer by mocked list

--- src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,11 +1,18 @@
 package com.microservices.chapter3
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class CustomerController{
-    @RequestMapping(value = "/customers", method =[(RequestMethod.GET)])
-    fun get() = Customer(1, "Kotlin")
+
+    @Autowired
+    lateinit var customers : ConcurrentHashMap<Int, Customer>
+
+    @RequestMapping(value = "/customer/{id}", method =[(RequestMethod.GET)])
+    fun getCustomer(@PathVariable id : Int) = customers[id]
 }
