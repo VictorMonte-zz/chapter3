2c32ec152e0a455ee9cbfcc1843d3cea9c90da9a
Victor Monte <victorbcm@hotmail.com>
2018-07-10 21:30:41 -0300

    Add filter naming

--- src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,10 +1,7 @@
 package com.microservices.chapter3
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
 @RestController
@@ -14,7 +11,10 @@ class CustomerController{
     lateinit var customers : ConcurrentHashMap<Int, Customer>
 
     @RequestMapping(value = "/customers", method = [(RequestMethod.GET)])
-    fun getCustomers() = customers.toList()
+    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter : String) =
+            customers.filter {
+                it.value.name.contains(nameFilter, true)
+            }.map(Map.Entry<Int, Customer>::value).toList()
 
     @RequestMapping(value = "/customer/{id}", method =[(RequestMethod.GET)])
     fun getCustomer(@PathVariable id : Int) = customers[id]
