d87537812e63df769221acdb73fc5934d3bc2231
Victor Monte <victorbcm@hotmail.com>
2018-07-12 21:46:58 -0300

    Alter to response entity

--- src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,6 +1,8 @@
 package com.microservices.chapter3
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
@@ -12,17 +14,22 @@ class CustomerController{
 
     @GetMapping(value = "/customers")
     fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter : String) =
-            customerService.searchCustomer(nameFilter)
+            ResponseEntity(customerService.searchCustomer(nameFilter), HttpStatus.OK)
 
     @GetMapping(value = "/customer/{id}")
-    fun getCustomer(@PathVariable id : Int) = customerService.getCustomer(id)
+    fun getCustomer(@PathVariable id : Int) : ResponseEntity<Customer?>{
+        val customer = customerService.getCustomer(id)
+        val status = if (customer == null) HttpStatus.NOT_FOUND else HttpStatus.OK
+        return ResponseEntity(customer, status)
+    }
 
     @PostMapping(value = "/customer/")
-    fun createCustomer(@RequestBody customer : Customer) = customerService.createCustomer(customer)
+    fun createCustomer(@RequestBody customer : Customer) = ResponseEntity(customerService.createCustomer(customer), HttpStatus.CREATED)
 
     @DeleteMapping(value = "/customer/{id}")
-    fun deleteCustomer(@PathVariable id : Int) = customerService.deleteCustomer(id)
+    fun deleteCustomer(@PathVariable id : Int) = ResponseEntity(customerService.deleteCustomer(id), HttpStatus.OK)
 
     @PutMapping(value = "/customers/{id}")
-    fun updateCustomer(@PathVariable id : Int, @RequestBody customer: Customer) = customerService.updateCustomer(id, customer)
+    fun updateCustomer(@PathVariable id : Int, @RequestBody customer: Customer) =
+            ResponseEntity(customerService.updateCustomer(id, customer), HttpStatus.OK)
 }
